fa61e81d7cb046c22225a49a7af6b88dc2044ab6
1701c <alex@reventlow.net>
2018-11-02 13:34:29 -0500

    added post question screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,21 @@
 var trivia = {
   currentQ: 0,
   questions: [{
-    question: 'what class of ship was the uss enterprise NCC-1701?',
+    question: 'what class of ship was the uss enterprise<br>NCC-1701?',
     info: '<span class="blue-text">USS ENTERPRISE</span><br><span class="tan-text">CLASS:</span> ?<br><span class="tan-text">REGISTRY:</span> NCC-1701<br><span class="tan-text">OWNER:</span> UNITED FEDERATION OF PLANETS<br><span class="tan-text">OPERATOR:</span> STARFLEET/UESPA<br><span class="tan-text">STATUS:</span> DESTROYED (2285)',
-    image: 'assets/images/1701.jpg',
+    image: ['assets/images/1701.jpg','assets/images/1701schem.jpg'],
     choices: ['galaxy', 'constitution', 'excelsior', 'sovereign'],
     answer: 1
   },{
-    question: '2what class of ship was the uss enterprise NCC-1701?',
-    info: '<span class="blue-text">USS ENTERPRISE</span><br><span class="tan-text">CLASS:</span> ?<br><span class="tan-text">REGISTRY:</span> NCC-1701<br><span class="tan-text">OWNER:</span> UNITED FEDERATION OF PLANETS<br><span class="tan-text">OPERATOR:</span> STARFLEET/UESPA<br><span class="tan-text">STATUS:</span> DESTROYED (2285)',
-    image: 'assets/images/1701.jpg',
-    choices: ['galaxy', 'constitution', 'excelsior', 'sovereign'],
-    answer: 1
+    question: 'WHO DESTROYED THE ENTERPRISE NCC-1701?',
+    info: '<span class="blue-text">USS ENTERPRISE</span><br><span class="tan-text">CLASS:</span> CONSTITUTION<br><span class="tan-text">REGISTRY:</span> NCC-1701<br><span class="tan-text">OWNER:</span> UNITED FEDERATION OF PLANETS<br><span class="tan-text">OPERATOR:</span> STARFLEET/UESPA<br><span class="tan-text">STATUS:</span> DESTROYED (2285)',
+    image: ['assets/images/1701spacedock.jpg','assets/images/1701destructs.jpg'],
+    choices: ['ROMULANS', 'BORG', 'KIRK', 'KHAN'],
+    answer: 2
   },{
     question: '3what class of ship was the uss enterprise NCC-1701?',
     info: '<span class="blue-text">USS ENTERPRISE</span><br><span class="tan-text">CLASS:</span> ?<br><span class="tan-text">REGISTRY:</span> NCC-1701<br><span class="tan-text">OWNER:</span> UNITED FEDERATION OF PLANETS<br><span class="tan-text">OPERATOR:</span> STARFLEET/UESPA<br><span class="tan-text">STATUS:</span> DESTROYED (2285)',
-    image: 'assets/images/1701.jpg',
+    image: ['assets/images/1701.jpg','assets/images/1701.jpg'],
     choices: ['galaxy', 'constitution', 'excelsior', 'sovereign'],
     answer: 1
   },{
@@ -63,9 +63,11 @@ var trivia = {
   }], 
 
   init: function () {
-    game.drawFakeButtons(4, '#fakeButtonDiv', true);
+    game.loginAttempts = 0;
+    game.correct = 0;
+    game.incorrect = 0;
     game.drawFakeButtons(14, '#buttonDiv', true);
-    $('#topLeftScreen').html('<br><h2>COMMAND LEVEL OVERRIDE <br><span class="lt-blue-text">INITIATED</span></h2>');
+    $('#topLeftScreen').html('<br><h2>COMMAND LEVEL <span class="tan-text">OVERRIDE</span><br><span class="lt-blue-text">INITIATED</span></h2>');
     $('#imageDiv, #infoDiv').empty();
     $('#questionText').html('<h3>UNAUTHORIZED ACCESS DETECTED<br>ENTER COMMAND CODES<br></h3>')
       .append('<h3><input id="passwordBox" type="password"></input><br></h3>')
@@ -83,7 +85,7 @@ var game = {
   correct: 0,
   incorrect: 0,
 
-  genericButton: function (isBlank) { // true returns blank, false returns not blank, undefined randomly returns blank
+  genericButton: function (isBlank) { // true returns blank, false returns not blank, undefined randomly returns blanks and blinking css class
     if ((isBlank === true) || ((isBlank === undefined) && ([Math.floor(Math.random() * 5)] == 0))) {
       return ($('<button type="button" class="btn black-back">X</button>'));
     } else {
@@ -108,7 +110,7 @@ var game = {
     }
   },
 
-  linkButton: function (buttonText, buttonClass) { // returns random color buttons with classes
+  linkButton: function (buttonText, buttonClass) { // returns random color buttons with classes 
     var buttonHTML = $('<button type="button" class="btn">');
     buttonHTML.addClass(this.colors[Math.floor(Math.random() * 8)] + '-back')
       .text(buttonText)
@@ -119,8 +121,6 @@ var game = {
   },
 
   login: function () { // login prompts
-    // game.drawFakeButtons(4, '#fakeButtonDiv', true);
-    // game.drawFakeButtons(12, '#buttonDiv', true);
     $('#passwordBox').val('');
     switch (this.loginAttempts) {
       case 0:
@@ -133,7 +133,7 @@ var game = {
         $('#questionText').html('<div style="padding-bottom:20px;">YOU HAVE EXCEEDED THE MAXIMUM NUMBER OF ATTEMPTED COMMAND CODES<br> <br>IDENTITY CONFIRMATION REQUIRED<br></div>')          
           .append(game.linkButton('ENGAGE', 'engageButton'));
         $('.engageButton').click(function () {
-          $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="lt-blue-text">INITIATED</span></h2>');
+          $('#topLeftScreen').html('<br><h2>COMMAND CODE <span class="tan-text">RESET</span><br><span class="lt-blue-text">INITIATED</span></h2>');
           game.drawQ()
         })
     }
@@ -141,7 +141,7 @@ var game = {
   },
 
   validateAnswer: function (a) {    
-    var nextSeconds = 4;
+    var nextSeconds = 3;
     var nextTimer = setInterval(nextTicker, 1000);
     clearInterval(this.drawQ.timer);
     $('.answer').off('click');
@@ -158,30 +158,43 @@ var game = {
         $('#' + i).attr('style', 'visibility:hidden;');
       } 
     } 
+    $('#imageDiv, #infoDiv').empty()
+    $('#questionText').html('<img src="' + trivia.questions[trivia.currentQ].image[1] + '" width="300">')
     if (a == trivia.questions[trivia.currentQ].answer) { 
       this.correct ++;
-      $('#questionText').append('<br>correct');
+      $('#questionText').append('<br> <br><span class="blinking blue-text">CORRECT</span>');
     } else {
       this.incorrect ++;
-      $('#questionText').append('<br>incorrect');
+      $('#questionText').append('<br> <br><span class="blinking red-text">INCORRECT</span>');
     }
     trivia.currentQ++;
-    $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="blue-text">' + this.correct + '</span>/<span class="lt-red-text">' + this.incorrect + '</span> &nbsp <span class="lt-blue-text">'+ (trivia.currentQ * 10) +'% COMPLETE</span></h2>');
+    $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="blue-text">' + this.correct + '</span>:<span class="lt-red-text">' + this.incorrect + '</span> &nbsp <span class="lt-blue-text">'+ (trivia.currentQ * 10) +'% COMPLETE</span></h2>');
     $('#timerDiv').html('NEXT QUESTION IN<br> &nbsp;');
   },
   
   endGame: function () {
-    if (this.correct > 8) {
-      $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="lt-blue-text">COMPLETE</span></h2>');
+    if (this.correct > 6) {
+      if (this.correct > 8) {
+        $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="lt-blue-text">COMPLETE</span></h2>')
+        $('#questionText').html('<h3><br> <br>COMMAND CODES SUCCESSFULLY RESET</h3><h3>YOUR NEW COMMAND CODES HAVE BEEN SENT TO YOU<br> <br></h3>')
+      } else {
+        $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="lt-red-text">INCONCLUSIVE</span></h2>')
+        $('#questionText').html('<h3><br> <br>UNABLE TO CONFIRM IDENTITY</h3><h3>YOU MAY ATTEMPT AGAIN<br> <br></h3>')         
+      }
+      $('#questionText').append(game.linkButton('ENGAGE', 'engageButton'))
+      $('.engageButton').click(function () {trivia.init()})
+        game.drawFakeButtons(14, '#buttonDiv', true);
     } else {
-      $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="red-text blinking">FAILED</span></h2>');
+      $('#topLeftScreen').html('<br><h2>COMMAND CODE RESET<br><span class="red-text blinking">FAILED</span></h2>')
+      $('#bottomDiv').empty()
+        .attr('style','margin-left: 120px;')
+        .append('<div id="endDiv">')
+      $('#endDiv').attr('style','margin-top: 20px; text-align: center; background-image: url("../assets/images/fedlogo.jpeg"); background-repeat: no-repeat; background-position: center; ')
+        .html('<h2 class="red-text blinking"><br>COMMAND CODE RESET FAILED</h2><br><h2>THIS CONSOLE HAS BEEN LOCKED</h2><h2>SECURITY HAS BEEN ALERTED</h2><br><h2 class="blinking">REMAIN IN THE AREA<br> <br></h2>')
+      $('#buttonDiv, #fakeButtonDiv').empty()
     }
-    $('#imageDiv, #infoDiv').empty();
+    $('#imageDiv, #infoDiv').empty()
     $('#timerDiv').attr('style', 'display:none;')
-    
-    game.drawFakeButtons(4, '#fakeButtonDiv', true);
-    game.drawFakeButtons(12, '#buttonDiv', true);
-    $('#questionText').html('<h3>COMMAND CODES SUCCESSFULLY CHANGED</h3><h3>YOUR NEW COMMAND CODES HAVE BEEN SENT TO YOU</h3>');
   },
 
   drawQ: function () {
@@ -201,9 +214,6 @@ var game = {
         seconds--;
         $('#timerDiv').html('TIME REMAINING<div class="blinking"> 00 : 00 : 0' + seconds + '<div>');
         if (seconds === 0) { stopTimer(false);
-          // clearInterval(timer);
-          // $('.answer').off('click');
-          // game.validateAnswer(false);
         } 
       }
       $('#buttonDiv').empty();
@@ -214,16 +224,12 @@ var game = {
           .append(this.genericButton(true));
         }
       $('.answer').on('click', function () { stopTimer(this.id);
-        // clearInterval(timer)
-        // $('.answer').off('click');
-        // game.validateAnswer(this.id);
       });    
-      $('#imageDiv').html('<img id="questionImg" src="' + trivia.questions[trivia.currentQ].image + '">');
+      $('#imageDiv').html('<img id="questionImg" src="' + trivia.questions[trivia.currentQ].image[0] + '">');
       $('#infoDiv').html(trivia.questions[trivia.currentQ].info);
       $('#questionText').html(trivia.questions[trivia.currentQ].question.toUpperCase());
       $('#timerDiv').html('TIME REMAINING<div class="blinking"> 00 : 00 : 0' + seconds + '</div>');
       $('#timerDiv').attr('style', '')
-      game.drawFakeButtons(4, '#fakeButtonDiv', true);
     }
   }
 };
